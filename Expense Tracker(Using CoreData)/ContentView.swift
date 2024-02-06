ab11dd720962eba0d9ca11c71cc9e6c59bee2de9
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-06 14:22:42 +0530

    Initial Commit

---
+++ Expense Tracker(Using CoreData)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Tracker(Using CoreData)
+//
+//  Created by sabiq on 06/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Tracker(Using CoreData)/Expense_Tracker_Using_CoreData_App.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Tracker_Using_CoreData_App.swift
+//  Expense Tracker(Using CoreData)
+//
+//  Created by sabiq on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Tracker_Using_CoreData_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
